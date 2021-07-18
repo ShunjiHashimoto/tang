6cdf822aa818478d52252e2e29b24291611932f5
ShunjiHashimoto <h.shunji1206@gmail.com>
2021-07-18 17:04:51 +0900

    add new teleop mode

--- cugo_teleop/scripts/cubase.py
+++ cugo_teleop/scripts/cubase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-## object_detection_Cubase & tele_operation!　##
+## object_detection_cubase & tele_operation!　##
 
 import rospy
 import time
@@ -31,81 +31,130 @@ p_r.start(0)
 p_l.start(0)
 
 BTN_BACK = 0x0100
+BTN_Y = 0x0001
+BTN_A = 0x0002
 AXS_MAX = 1.0
 AXS_OFF = 0.0
 
-class cubase():
+class CugoController():
     def __init__(self):
         self.message = "stop"
         self.btn = 0
         self.main = 0
-        self.lud = 0
-        self.rud = 0
+        self.joy_l = 0
+        self.joy_r = 0
+        self.percent = 80
 
         # subscribe to motor messages on topic "msg_topic"
-        self._msg_sub = rospy.Subscriber("/msg_topic", String, self.msg_callback, queue_size=1)
+        self.str_sub = rospy.Subscriber("/msg_topic", String, self.strCallback, queue_size=1)
         
         # subscribe to joystick messages on topic "joy"
-        self._joy_sub = rospy.Subscriber("joy", Joy, self.joy_callback, queue_size=1)
+        self.joy_sub = rospy.Subscriber("joy", Joy, self.joyCallback, queue_size=1)
         
-    def msg_listener(self):
-        if self.main == 0:
-            ### teleop start
-            
-            #value_0 = int(joy_msg.axes[0]*100)
-            value_1 = self.lud
-            #value_3 = int(joy_msg.axes[3]*100)
-            value_2 = self.rud
+    def modeChange(self):
+        if self.main == 1:
+            ### old_teleop start 
+            motor_l = self.joy_l
+            motor_r = self.joy_r
             
             time.sleep(0.1)
             
-            if value_1 > 0 and value_2 > 0:
+            if motor_l > 0 and motor_r > 0:
                 GPIO.output(ENABLE_r, GPIO.LOW)
                 GPIO.output(ENABLE_l, GPIO.LOW)
-                p_r.ChangeDutyCycle(value_1)
-                p_l.ChangeDutyCycle(value_2)
-                print("go:", value_1, value_2)
+                p_r.ChangeDutyCycle(motor_l)
+                p_l.ChangeDutyCycle(motor_r)
+                print("go:", motor_l, motor_r)
             
-            elif value_1 < 0 and value_2 < 0:
+            elif motor_l < 0 and motor_r < 0:
                 GPIO.output(ENABLE_r, GPIO.HIGH)
                 GPIO.output(ENABLE_l, GPIO.HIGH)
-                p_r.ChangeDutyCycle(-(value_1))
-                p_l.ChangeDutyCycle(-(value_2))
-                print("back:", value_1, value_2)
+                p_r.ChangeDutyCycle(-(motor_l))
+                p_l.ChangeDutyCycle(-(motor_r))
+                print("back:", motor_l, motor_r)
             
             else:
-                print("stop:", value_1, value_2)
+                print("stop:", motor_l, motor_r)
                 p_r.stop()
                 p_l.stop()
                 p_r.start(0)
                 p_l.start(0)
+
+        elif self.main == 0:
+            ### new_teleop start
+            if(self.joy_r == -0):
+                self.joy_r = 0
+            if(self.joy_l == -0):
+                self.joy_l = 0
+
+            motor_l = self.joy_l
+            motor_r = self.joy_r
+            max = self.percent
+            ## 右回り
+            if(self.joy_r < 0):
+                if(self.joy_l > 0):
+                    motor_r  += (self.joy_l * self.joy_r)/max
+                else:
+                    motor_r -= self.joy_r
+            ## 左回り
+            elif(self.joy_r > 0):
+                if(self.joy_l > 0):
+                    motor_l -= (self.joy_r * self.joy_l) / max 
+                else:
+                    motor_l += self.joy_r 
+            else:
+                motor_l = self.joy_l
+                motor_r = self.joy_l
+            time.sleep(0.1)
+            
+            if motor_l > 0 and motor_r > 0:
+                GPIO.output(ENABLE_r, GPIO.LOW)
+                GPIO.output(ENABLE_l, GPIO.LOW)
+                p_r.ChangeDutyCycle(motor_l)
+                p_l.ChangeDutyCycle(motor_r)
+                print("go:", motor_l, motor_r)
+            
+            elif motor_l < 0 and motor_r < 0:
+                GPIO.output(ENABLE_r, GPIO.HIGH)
+                GPIO.output(ENABLE_l, GPIO.HIGH)
+                p_r.ChangeDutyCycle(-(motor_l))
+                p_l.ChangeDutyCycle(-(motor_r))
+                print("back:", motor_l, motor_r)
+            
+            else:
+                print("stop:", motor_l, motor_r)
+                p_r.stop()
+                p_l.stop()
+                p_r.start(0)
+                p_l.start(0)
+
         else:
             if self.message == "go ahead":
                 print("go ahead")
-                value_1 = 80.0
-                value_2 = 80.0
+                motor_l = 80.0
+                motor_r = 80.0
                 GPIO.output(ENABLE_r, GPIO.LOW)
                 GPIO.output(ENABLE_l, GPIO.LOW)
-                p_r.ChangeDutyCycle(value_1)
-                p_l.ChangeDutyCycle(value_2)
+                p_r.ChangeDutyCycle(motor_l)
+                p_l.ChangeDutyCycle(motor_r)
                 
             elif self.message == "turn right":
                 print("turn right")
-                value_1 = 80.0
-                value_2 = 10.0
+                motor_l = 80.0
+                motor_r = 10.0
                 GPIO.output(ENABLE_r, GPIO.LOW)
                 GPIO.output(ENABLE_l, GPIO.LOW)
-                p_r.ChangeDutyCycle(value_1)
-                p_l.ChangeDutyCycle(value_2)
+                p_r.ChangeDutyCycle(motor_l)
+                p_l.ChangeDutyCycle(motor_r)
                 
             elif self.message == "turn left":
                 print("turn left")
-                value_1 = 10.0
-                value_2 = 80.0
+                motor_l = 10.0
+                motor_r = 80.0
                 GPIO.output(ENABLE_r, GPIO.LOW)
                 GPIO.output(ENABLE_l, GPIO.LOW)
-                p_r.ChangeDutyCycle(value_1)
-                p_l.ChangeDutyCycle(value_2)
+                p_r.ChangeDutyCycle(motor_l)
+                p_l.ChangeDutyCycle(motor_r)
                 
             elif self.message == "stop":
                 print("stop")
@@ -117,50 +166,59 @@ class cubase():
                 print("error")
                 #time.sleep(0.1)
     
-    def msg_callback(self, msg):
+    def strCallback(self, msg):
         self.message = msg.data
         
-    def joy_callback(self, joy_msg):
+    def joyCallback(self, joy_msg):
         newbtn = 0
         
         if(joy_msg.buttons[6]):
             newbtn |= BTN_BACK
         
-        ldu = joy_msg.axes[1]
-        if(ldu <= -AXS_OFF):
-            ldu += AXS_OFF
-        elif(ldu >= AXS_OFF):
-            ldu -= AXS_OFF
+        joy_l = joy_msg.axes[1]
+        if(joy_l <= -AXS_OFF):
+            joy_l += AXS_OFF
+        elif(joy_l >= AXS_OFF):
+            joy_l -= AXS_OFF
         else:
-            ldu = 0
+            joy_l = 0
             
-        rdu = joy_msg.axes[4]
-        if(rdu <= -AXS_OFF):
-            rdu += AXS_OFF
-        elif(rdu >= AXS_OFF):
-            rdu -= AXS_OFF
+        joy_r = joy_msg.axes[4]
+        if(joy_r <= -AXS_OFF):
+            joy_r += AXS_OFF
+        elif(joy_r >= AXS_OFF):
+            joy_r -= AXS_OFF
         else:
-            rdu = 0
+            joy_r = 0
         
-        self.lud = int(100 * ldu / (AXS_MAX - AXS_OFF))
-        self.rud = int(100 * rdu / (AXS_MAX - AXS_OFF))
+        self.joy_l = int(self.percent * joy_l / (AXS_MAX - AXS_OFF))
+        self.joy_r = int(self.percent * joy_r / (AXS_MAX - AXS_OFF))
             
         push = ((~self.btn) & newbtn)
         self.btn = newbtn
         if(push & BTN_BACK):
-            self.main = (self.main + 1)%2
+            self.main = (self.main + 1)%3
+        elif(push & BTN_Y):
+            self.percent += 10
+        elif(push & BTN_A):
+            self.percent -= 10
+            
+        if(self.percent > 100):
+            self.percent = 100
+        elif(self.percent < 10):
+            self.percent = 10
 
 def main():
     # start node
     rospy.init_node("cubase", anonymous=True)
         
-    instance = cubase()
+    instance = CugoController()
  
     # ratesleep
     rate = rospy.Rate(40)
     
     while not rospy.is_shutdown():
-        instance.msg_listener()
+        instance.modeChange()
         rate.sleep()
         
     # spin
@@ -169,4 +227,3 @@ def main():
 if __name__ == '__main__':
     main()
     GPIO.cleanup()
-
--- cugo_teleop/scripts/cubase_teleop.py
+++ cugo_teleop/scripts/cubase_teleop.py
@@ -1,94 +1,138 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-## teleoperation_Cubase!　##
+## object_detection_cubase & tele_operation!　##
 
 import rospy
+import time
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
-import time
-import RPi.GPIO as GPIO
+from std_msgs.msg import String
 
-
-R = 12
-L = 13
-DUTY = 50.0
-ENABLE_r = 17
-ENABLE_l = 18
 top = 1000
 bottom = 50
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(R, GPIO.OUT)
-GPIO.setup(L, GPIO.OUT)
-GPIO.setup(ENABLE_r, GPIO.OUT)
-GPIO.setup(ENABLE_l, GPIO.OUT)
-GPIO.output(ENABLE_r, GPIO.LOW)
-GPIO.output(ENABLE_l, GPIO.LOW)
+R = 12
+L = 13
+ENABLE_r = 17
+ENABLE_l = 18
 
-p_r = GPIO.PWM(R, bottom)
-p_l = GPIO.PWM(L, bottom)
+BTN_BACK = 0x0100
+BTN_Y = 0x0001
+BTN_A = 0x0002
+AXS_MAX = 1.0
+AXS_OFF = 0.0
+# y = button[3], a = button[0]
+class CugoController():
+    def __init__(self):
+        self.message = "stop"
+        self.btn = 0
+        self.main = 0
+        self.joy_l = 0
+        self.joy_r = 0
+        self.percent = 50
+        self.str_sub = rospy.Subscriber("/msg_topic", String, self.strCallback, queue_size=1)
+        self.joy_sub = rospy.Subscriber("joy", Joy, self.joyCallback, queue_size=1)
+        
+    def modeChange(self):
+        if self.main == 0:
+            ### old_teleop start 
+            print("old_teleop mode")
 
+        elif self.main == 1:
+            ### new_teleop start
+            print("new_teleop mode")
+            if(self.joy_r == -0):
+                self.joy_r = 0
+            if(self.joy_l == -0):
+                self.joy_l = 0
 
-p_r.start(0)
-p_l.start(0)
+            motor_l = self.joy_l
+            motor_r = self.joy_l
+            max = self.percent
+            ## 右回り
+            if(self.joy_r < 0):
+                if(self.joy_l > 0):
+                    motor_r  += (self.joy_l * self.joy_r)/max
+                else:
+                    motor_r -= self.joy_r
+            ## 左回り
+            elif(self.joy_r > 0):
+                if(self.joy_l > 0):
+                    motor_l -= (self.joy_r * self.joy_l) / max 
+                else:
+                    motor_l += self.joy_r 
+            else:
+                motor_l = self.joy_l
+                motor_r = self.joy_l
 
+            time.sleep(0.1)
+            print("motor_value: motor_l, motor_r", motor_l, motor_r)
+            return
 
-def joy_listener():
+        else:
+            print("object_detection mode")
+    
+    def strCallback(self, msg):
+        self.message = msg.data
+        
+    def joyCallback(self, joy_msg):
+        newbtn = 0
+        
+        if(joy_msg.buttons[6]):
+            newbtn |= BTN_BACK
+        elif(joy_msg.buttons[0]):
+            newbtn |= BTN_A
+        elif(joy_msg.buttons[3]):
+            newbtn |= BTN_Y
+        
+        joy_l = joy_msg.axes[1]
+        if(joy_l <= -AXS_OFF):
+            joy_l += AXS_OFF
+        elif(joy_l >= AXS_OFF):
+            joy_l -= AXS_OFF
+        else:
+            joy_l = 0
+            
+        joy_r = joy_msg.axes[3]
+        if(joy_r <= -AXS_OFF):
+            joy_r += AXS_OFF
+        elif(joy_r >= AXS_OFF):
+            joy_r -= AXS_OFF
+        else:
+            joy_r = 0
+        
+        self.joy_l = int(self.percent * joy_l / (AXS_MAX - AXS_OFF))
+        self.joy_r = int(self.percent * joy_r / (AXS_MAX - AXS_OFF))
+
+        push = ((~self.btn) & newbtn)
+        self.btn = newbtn
+        if(push & BTN_BACK):
+            self.main = (self.main + 1)%3
+        elif(push & BTN_Y):
+            self.percent += 10
+        elif(push & BTN_A):
+            self.percent -= 10
+            
+        if(self.percent > 100):
+            self.percent = 100
+        elif(self.percent < 10):
+            self.percent = 10
+
+def main():
     # start node
-    rospy.init_node("joy_twist", anonymous=True)
-
-    # subscribe to joystick messages on topic "joy"
-    rospy.Subscriber("joy", Joy, joy_callback, queue_size=1)
-
-    # keep node alive until stopped
+    rospy.init_node("cubase", anonymous=True)
+        
+    instance = CugoController()
+ 
+    # ratesleep
+    rate = rospy.Rate(40)
+    
+    while not rospy.is_shutdown():
+        instance.modeChange()
+        rate.sleep()
+        
+    # spin
     rospy.spin()
-
-# called when joy message is received
-
-
-def joy_callback(joy_msg):
-    #value_0 = int(joy_msg.axes[0]*100)
-    value_1 = int(joy_msg.axes[1] * 100)
-    #value_3 = int(joy_msg.axes[3]*100)
-    value_2 = int(joy_msg.axes[4] * 100)
-
-    time.sleep(0.1)
-
-    if value_1 == -0:
-        value_1 = 0
-
-    if value_2 == -0:
-        value_2 = 0
-
-    if value_1 > 0 and value_2 > 0:
-        # GPIO.output(ENABLE_r, GPIO.LOW)
-        # GPIO.output(ENABLE_l, GPIO.LOW)
-        # p_r.ChangeDutyCycle(value_1)
-        # p_l.ChangeDutyCycle(value_2)
-        print("go:", value_1, value_2)
-
-    elif value_1 < 0 and value_2 < 0:
-        # GPIO.output(ENABLE_r, GPIO.HIGH)
-        # GPIO.output(ENABLE_l, GPIO.HIGH)
-        # p_r.ChangeDutyCycle(-(value_1))
-        # p_l.ChangeDutyCycle(-(value_2))
-        print("back:", value_1, value_2)
-
-    else:
-        print("stop:", value_1, value_2)
-        # p_r.stop()
-        # p_l.stop()
-        # p_r.start(0)
-        # p_l.start(0)
-
-
+                
 if __name__ == '__main__':
-    try:
-        joy_listener()
-    except rospy.ROSInterruptException:
-        pass
-
-  # p_r.stop()
-  # p_l.stop()
-
-GPIO.cleanup()
+    main()
