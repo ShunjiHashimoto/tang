8c721ec8f43b6f429096656f1579e9b27a06f49d
ShunjiHashimoto <h.shunji1206@gmail.com>
2021-10-28 05:20:11 +0900

    gpiono

--- tang_teleop/scripts/gpio.py
+++ tang_teleop/scripts/gpio.py
@@ -7,14 +7,14 @@ import RPi.GPIO as GPIO
 
 def main():
     # modeを選択
-    GPIO.setmode(GPIO.BOARD)
-    gpio_pin_l = 11
-    gpio_pin_r = 12
-#     GPIO.setup(gpio_pin_r, GPIO.OUT)
-#     GPIO.setup(gpio_pin_l, GPIO.OUT)
+    GPIO.setmode(GPIO.BCM)
+    gpio_pin_l = 17
+    gpio_pin_r = 18
+    GPIO.setup(gpio_pin_r, GPIO.OUT)
+    GPIO.setup(gpio_pin_l, GPIO.OUT)
 
-    # アナログピン
-    pwm_pin_r = 33
+    # アナログピン, right)ANA2:33, left)ANA1:32
+    pwm_pin_r = 13
     GPIO.setup(pwm_pin_r, GPIO.OUT)
     p_r = GPIO.PWM(pwm_pin_r, 50)
     p_r.start(0)
@@ -24,8 +24,8 @@ def main():
         # デジタル出力ピンを設定, 回転方向を決められる
         # DIG1 = 11(LEFT), DIG2 = 12(RIGHT)
         print("gpio high")
-        # GPIO.output(gpio_pin_r, GPIO.HIGH)
-        # GPIO.output(gpio_pin_l, GPIO.HIGH)
+        GPIO.output(gpio_pin_r, GPIO.HIGH)
+        GPIO.output(gpio_pin_l, GPIO.HIGH)
         p_r.ChangeDutyCycle(40)
         time.sleep(6)
 
@@ -34,8 +34,8 @@ def main():
         time.sleep(1)
 
         p_r.start(0)
-        # GPIO.output(gpio_pin_r, GPIO.LOW)
-        # GPIO.output(gpio_pin_l, GPIO.LOW)
+        GPIO.output(gpio_pin_r, GPIO.LOW)
+        GPIO.output(gpio_pin_l, GPIO.LOW)
         print("gpio low")
         p_r.ChangeDutyCycle(40)
         time.sleep(6)
