5593f8b316ae842ba765a1607e7af8100966e777
Shunji Hashimoto <h.shunji1206@gmail.com>
2022-01-27 12:23:40 +0900

    yamlテストをするためにテストスクリプト追加した

--- tang_detection/scripts/utils/yaml_test.py
+++ tang_detection/scripts/utils/yaml_test.py
@@ -13,15 +13,13 @@ def main():
     rospy.init_node('yaml_test', anonymous=True)
     rospy.loginfo("start yaml_test node")
     pkg_name = 'tang_detection'
-    list = rosparam.load_file(roslib.packages.get_pkg_dir(pkg_name) + "/config/yaml_test.yaml")[0][0]
-    right_point1 = list[0]
-    print(len(right_point1))
+    list = rosparam.load_file(roslib.packages.get_pkg_dir(pkg_name) + "/config/yaml_test.yaml")
+    # [0][0]["waist_rotation_z"]
     cnt = 0
     place_max_cnt = 8
-    while(not rospy.is_shutdown()):
-        place_point = list[cnt%2][(math.floor(cnt/2))%place_max_cnt]
-        cnt += 1
-        print(place_point[0], place_point[1])
+    # while(not rospy.is_shutdown()):
+    print(list)
+    cnt += 1
         # time.sleep(0.01)
 
 if __name__ == '__main__':
