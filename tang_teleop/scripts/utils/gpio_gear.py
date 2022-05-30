f5a294302e71326e542dc84073ffde8f63df346e
ShunjiHashimoto <h.shunji1206@gmail.com>
2022-05-30 23:45:43 +0000

    gearの回転数をカウントし、速度を求める

---
+++ tang_teleop/scripts/utils/gpio_gear.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# import rospy
+import sys
+import time
+import RPi.GPIO as GPIO
+import lcd_display
+# circle pitch
+Pitch = 6.1850105367549055
+cnt_list = [0, 0]
+
+def add_right_gear_count(channel):
+    cnt_list[0]+=1
+
+def add_left_gear_count(channel):
+    cnt_list[1]+=1
+
+def calc_velocity(count, time):
+    return Pitch*count/time
+
+def main():
+    GPIO.setmode(GPIO.BCM)
+    gpio_pin_r = 22
+    GPIO.setup(gpio_pin_r, GPIO.OUT)
+    GPIO.setup(11, GPIO.OUT, initial = GPIO.LOW)
+    GPIO.setup(19, GPIO.OUT, initial = GPIO.LOW)
+    GPIO.setup(21, GPIO.OUT, initial = GPIO.LOW)
+    GPIO.setup(16, GPIO.OUT, initial = GPIO.LOW)
+
+    # OUTPUT_GPIO = 27 # LEDに接続するGPIO番号
+
+    # アナログピン, ANA2(RIGHT) = 32
+    pwm_pin_r = 12
+    GPIO.setup(pwm_pin_r, GPIO.OUT)
+    p_r = GPIO.PWM(pwm_pin_r, 40)
+    p_r.start(0)
+
+    lcd = lcd_display.lcd()
+    t = 0.1
+
+    # 割り込みイベント設定
+    right_gear_pin = 17
+    left_gear_pin = 27
+    GPIO.setup(right_gear_pin, GPIO.IN)
+    GPIO.setup(left_gear_pin, GPIO.IN)
+    # bouncetimeは割り込みを行った後設定された時間は割り込みを検知しないという糸
+    GPIO.add_event_detect(right_gear_pin, GPIO.RISING, bouncetime=1)
+    GPIO.add_event_detect(left_gear_pin, GPIO.RISING, bouncetime=1)
+    # コールバック関数登録
+    GPIO.add_event_callback(right_gear_pin, add_right_gear_count) 
+    GPIO.add_event_callback(left_gear_pin, add_left_gear_count) 
+
+    
+    try:
+        while (1):
+            vel_r = calc_velocity(cnt_list[0], t)
+            vel_l = calc_velocity(cnt_list[1], t)
+            print( '\033[31m'+'右車輪の速度: '+'\033[0m'+str(vel_r))
+            print( '\033[32m'+'左車輪の速度: '+'\033[0m'+str(vel_l))
+            cnt_list[0] = cnt_list[1] = 0
+            time.sleep(t)
+    finally:
+        p_r.stop()
+        GPIO.cleanup()
+        # lcd.lcd_clear()
+
+if __name__ == '__main__':
+    main()
+    GPIO.cleanup()
--- tang_teleop/scripts/utils/gpio_toggle.py
+++ tang_teleop/scripts/utils/gpio_toggle.py
@@ -1,28 +1,20 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Aug  2 08:14:04 2020
-
-@author: Souichirou Kikuchi
-"""
 
 import RPi.GPIO as GPIO
 from time import sleep
 
-LED_GPIO = 4 # LEDに接続するGPIO番号
-TACT_GPIO = 17 # Tack Switchに接続するGPIO番号
+OUTPUT_GPIO = 27 # LEDに接続するGPIO番号
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_GPIO, GPIO.OUT)
-GPIO.setup(TACT_GPIO, GPIO.IN)
+GPIO.setup(OUTPUT_GPIO, GPIO.IN)
 
 try:
     print('--- start program ---')
     while True:
-        if GPIO.input(TACT_GPIO) == GPIO.HIGH:
-            GPIO.output(LED_GPIO, GPIO.HIGH)
-        else:
-            GPIO.output(LED_GPIO, GPIO.LOW)
-        sleep(0.01)
+        result = GPIO.input(OUTPUT_GPIO)
+        print(result)
+        # sleep(0.01)
 except KeyboardInterrupt:
     pass
 finally:
