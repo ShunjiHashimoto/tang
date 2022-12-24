3a16fbb0c9a53502d61ab8a69ef249e02ac22c1d
ShunjiHashimoto <h.shunji1206@gmail.com>
2022-12-24 07:06:03 +0000

    Add atom lite code

---
+++ tang_detection/scripts/atom_lite.py
@@ -0,0 +1,20 @@
+import serial
+import time
+
+usb_device = serial.Serial('/dev/ttyUSB0', '9600', timeout=1.0)
+
+def main():
+    command = "HumnFollow"
+    while(True):
+        try:
+            print("Write signal")
+            usb_device.write(command.encode())
+            time.sleep(1.0)
+        except:
+            print("close usb_device", flush=True)
+            usb_device.close()
+            return
+
+if __name__ == "__main__":
+    main()
+    
