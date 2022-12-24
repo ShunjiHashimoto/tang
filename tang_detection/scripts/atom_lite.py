4af8c9ded76372f635e460d6a928cab20813cbb8
ShunjiHashimoto <h.shunji1206@gmail.com>
2022-12-24 16:51:51 +0000

    add uart with atomlite

--- tang_detection/scripts/atom_lite.py
+++ tang_detection/scripts/atom_lite.py
@@ -4,7 +4,7 @@ import time
 usb_device = serial.Serial('/dev/ttyUSB0', '9600', timeout=1.0)
 
 def main():
-    command = "HumnFollow"
+    command = "Manual"
     while(True):
         try:
             print("Write signal")
