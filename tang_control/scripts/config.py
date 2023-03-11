b5e7b2bf28949812cdfcb50a3b057a7ab0997bb1
ShunjiHashimoto <h.shunji1206@gmail.com>
2023-03-11 23:44:05 +0900

    refactoring

---
+++ tang_control/scripts/config.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Pin:
+    r_direction = 18
+    l_direction = 17
+    teleop_mode = 21
+    follow_mode = 16
+    r_pwm = 13
+    l_pwm = 12
+    vrx_channel = 0
+    vry_channel = 1
+    swt_channel = 2
+
+class PID:
+    p_gain = 15.0 
+    d_gain = 7.0
+    #d_gain = 7.0
+    dt = 0.1
+
+class PWM:
+    freq = 1000
+
+class HumanFollowParam:
+    depth_min_thresh = 0.8
