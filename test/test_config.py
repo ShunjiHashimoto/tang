9b362a4ba1016ef881475af016638dae4ddc26d2
ShunjiHashimoto <h.shunji1206@gmail.com>
2023-10-09 14:40:05 +0900

    firstcommit add test

---
+++ test/test_config.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import math
+
+class Pin:
+    encoder_l_A  = 5
+    encoder_l_B  = 6
+    direction_l = 18
+    pwm_l        = 13 
+    encoder_r_A  = 22
+    encoder_r_B  = 27
+    direction_r = 17
+    pwm_r        = 12
+    teleop_mode = 21
+    follow_mode = 16
+
+class FOLLOWPID:
+    p_gain = 0.01
+    d_gain = 0.0
+    #d_gain = 7.0
+    dt = 0.1
+
+class PID:
+    Kp = 1.0
+    Ki = 0.01
+    Kd = 0.00
+    dt = 0.005 # 0.0001がmax
+    
+class PWM:
+    # PWM周波数をHzで指定
+    freq = 1000 # [Hz]
+    max_duty = 50
+    
+class Fig:
+    time_data  = []
+    vel_data = []
+    w_data = []
+    target_vel_data = []
+    target_w_data = []
+    target_a_data = []
+
+class Control:
+    # 入力電圧
+    input_v = 27
+    # 目標角速度
+    w_target = 0.0001
+    # 目標速度
+    v_target = 0.3
+    # 目標加速度
+    a_target = 0.01
+    # 目標角加速度
+    alpha_target = 0.01
+    # 逆起電圧定数
+    Ke_r = 0.5905
+    Ke_l = 0.5905
+    # トルク定数
+    Kt_r = 0.6666
+    Kt_l = 0.6666
+    # 巻線抵抗
+    R = 3.05931
+    # モータ１回転あたりのエンコーダ値
+    encoder_1rotation_r = 2150
+    encoder_1rotation_l = 1900
+    # エンコーダ値1あたりの回転角度[rad]
+    radian_1encoder_r = 2*math.pi/encoder_1rotation_r
+    radian_1encoder_l = 2*math.pi/encoder_1rotation_l
+    # モータの回転数
+    rotation_num = 2
+    # トレッド幅[m]
+    tread_w = 0.32
+    # 車輪半径[m]
+    wheel_r = 0.05
+    # 車体質量
+    M = 24.6
+    # 車体慣性モーメント J = ml^2
+    # J = 1/3(a^2 + b^2) 44cm, 40cm = 0.58999
+    J = M*(0.22*0.22 + 0.2*0.2)/3
+    
+class HumanFollowParam:
+    min_area_thresh = 70
+    max_area_thresh = 200
---
+++ test/test_vel_control.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+from datetime import datetime
+import pigpio
+import sys
+import matplotlib.pyplot as plt
+from test_config import Pin, PID, PWM, Fig, Control
+from datetime import datetime
+
+class Motor:
+    def __init__(self):
+        self.pi = pigpio.pi()
+        self.last_gpio_r = None
+        self.last_level_r = None
+        self.last_gpio_l = None
+        self.last_level_l = None
+        self.encoder_values = {'r' : 0, 'l' : 0}
+        self.prev_encoder_values = {'r' : 0, 'l' : 0}
+        self.prev_error = {'v' : 0, 'w' : 0}
+        self.error_sum = {'v' : 0, 'w' : 0}
+        self.prev_time = time.time()
+        self.prev_time_for_pwm = time.time()
+        self.set_gpio()
+        # プロット
+        self.fig = plt.figure()
+        self.ax1 = self.fig.add_subplot(1, 2, 1)
+        self.ax2 = self.fig.add_subplot(1, 2, 2)
+        # plt.xlabel("Time [s]")
+        # plt.ylabel("v[m/s]")
+        # plt.title("Target v and Current v")
+        # plt.ylim(0, 1)
+        
+    def set_gpio(self):
+        self.pi.set_mode(Pin.encoder_r_A, pigpio.INPUT)
+        self.pi.set_mode(Pin.encoder_r_B, pigpio.INPUT)
+        self.pi.set_mode(Pin.direction_r, pigpio.OUTPUT)
+        self.pi.set_mode(Pin.direction_l, pigpio.OUTPUT)
+        self.pi.write(Pin.direction_r, pigpio.HIGH)
+        self.pi.write(Pin.direction_l, pigpio.HIGH)
+        # コールバック関数登録
+        self.pi.set_pull_up_down(Pin.encoder_r_A, pigpio.PUD_UP)
+        self.pi.set_pull_up_down(Pin.encoder_r_B, pigpio.PUD_UP)
+        self.pi.callback(Pin.encoder_r_A, pigpio.EITHER_EDGE, self.get_encoder_turn_r) 
+        self.pi.callback(Pin.encoder_r_B, pigpio.EITHER_EDGE, self.get_encoder_turn_r) 
+        self.pi.set_pull_up_down(Pin.encoder_l_A, pigpio.PUD_UP)
+        self.pi.set_pull_up_down(Pin.encoder_l_B, pigpio.PUD_UP)
+        self.pi.callback(Pin.encoder_l_A, pigpio.EITHER_EDGE, self.get_encoder_turn_l) 
+        self.pi.callback(Pin.encoder_l_B, pigpio.EITHER_EDGE, self.get_encoder_turn_l) 
+
+    def get_encoder_turn_r(self, gpio, level, tick):
+        if gpio == self.last_gpio_r:
+            return
+        if gpio == Pin.encoder_r_A and level == 1:
+            self.encoder_values['r'] += 1 
+        self.last_gpio_r = gpio
+        self.last_level_r = level
+
+    def get_encoder_turn_l(self, gpio, level, tick):
+        if gpio == self.last_gpio_l:
+            return
+        if gpio == Pin.encoder_l_A and level == 1:
+            self.encoder_values['l'] += 1 
+        self.last_gpio_l = gpio
+        self.last_level_l = level
+
+    def calculate_duty_cycle(self, duty):
+        # 1,000,000で100％
+        return int(((duty/100) * 1000000))
+    
+    def calc_w(self, encoder_val, prev_encoder_val, dt):
+        delta_encoder_val = encoder_val - prev_encoder_val
+        return (delta_encoder_val*Control.radian_1encoder_r)/dt
+    
+    def pid_control(self, v_curr, w_curr, dt):
+        error_v  = Control.v_target - v_curr
+        error_w  = Control.w_target - w_curr
+        if(error_v < 0): error_v = error_v
+        pid_error_v = PID.Kp*error_v + PID.Ki*self.error_sum['v'] + PID.Kd*(error_v - self.prev_error['v'])/dt
+        pid_error_w = PID.Kp*error_w + PID.Ki*self.error_sum['w'] + PID.Kd*(error_w - self.prev_error['w'])/dt
+        print(f"\033[91mpid_error_v: {pid_error_v:.3f}, PID.Kp*error_v：{PID.Kp*error_v}, PID.Ki*self.error_sum['v']：{PID.Ki*self.error_sum['v']}, PID.Kd*(error_v - self.prev_error['v'])/dt：{PID.Kd*(error_v - self.prev_error['v'])/dt:.3f}\033[0m")
+        # print(f"\033[91merror_v: {error_v:.3f}, 目標速度：{Control.v_target}, 現在速度：{v_curr}, 計算後のerror_v: {pid_error_v:.3f}, error_sum: {self.error_sum['v']:.3f}, Dゲインの値{PID.Kd*(error_w - self.prev_error['w'])/dt:.3f}\033[0m")
+        self.error_sum['v'] += error_v
+        self.error_sum['w'] += error_w
+        self.prev_error['v'] = error_v
+        self.prev_error['w'] = error_w
+        return pid_error_v, pid_error_w
+    
+    def calc_target_w_i(self, v_target, w_target, a_target, alpha_target, dt):
+        # 角速度計算
+        measured_w_r = self.calc_w(self.encoder_values['r'], self.prev_encoder_values['r'], dt)
+        measured_w_l = self.calc_w(self.encoder_values['l'], self.prev_encoder_values['l'], dt)
+        # 車体の速度、角速度計算
+        v_est = (measured_w_r*Control.wheel_r + measured_w_l*Control.wheel_r)/2
+        w_est = (measured_w_r*Control.wheel_r - measured_w_l*Control.wheel_r)/Control.tread_w
+        # エンコーダ値保存
+        self.prev_encoder_values['r'] = self.encoder_values['r']
+        self.prev_encoder_values['l'] = self.encoder_values['l']
+        # PID制御
+        pid_error_v, pid_error_w = self.pid_control(v_est, w_est, dt)
+        print(f"\033[91m車体の推定目標速度: {v_est + pid_error_v}\033[0m")
+        # 各モータの角速度
+        w_r = (1/Control.wheel_r)*(v_est + pid_error_v) + (Control.tread_w/(2*Control.wheel_r)*(Control.w_target + pid_error_w))
+        w_l = (1/Control.wheel_r)*(v_est + pid_error_v) - (Control.tread_w/(2*Control.wheel_r)*(Control.w_target + pid_error_w))
+        # トルク計算
+        T_r = (Control.wheel_r/2)*Control.M*Control.a_target + (Control.wheel_r/Control.tread_w)*Control.J*Control.alpha_target
+        T_l = (Control.wheel_r/2)*Control.M*Control.a_target - (Control.wheel_r/Control.tread_w)*Control.J*Control.alpha_target
+        # 電流計算
+        i_r = T_r/Control.Kt_r
+        i_l = T_l/Control.Kt_l
+        # ログ
+        Fig.target_w_data.append(Control.w_target)
+        Fig.target_vel_data.append(Control.v_target)
+        Fig.vel_data.append(v_est)
+        Fig.w_data.append(w_est)
+        return w_r, w_l, i_r, i_l
+    
+    def cal_duty(self, w_r, w_l, i_r, i_l):
+        e_r = Control.Ke_r*w_r + Control.R*i_r
+        e_l = Control.Ke_l*w_l + Control.R*i_l
+        duty_r = 100*e_r/Control.input_v
+        duty_l = 100*e_l/Control.input_v
+        return duty_r, duty_l
+    
+    def pwm_control(self, pin, duty):
+        cnv_dutycycle = self.calculate_duty_cycle(duty)
+        self.pi.hardware_PWM(pin, PWM.freq, cnv_dutycycle)
+        return
+    
+    def motor_control(self, w_r, w_l, i_r, i_l):
+        # duty計算
+        duty_r, duty_l = self.cal_duty(w_r, w_l, i_r, i_l)
+        if duty_r<0: duty_r=0
+        if duty_l<0: duty_l=0
+        if duty_r > PWM.max_duty: duty_r = PWM.max_duty
+        if duty_l > PWM.max_duty: duty_l = PWM.max_duty
+        # PWM出力
+        self.pwm_control(Pin.pwm_r, duty_r)
+        self.pwm_control(Pin.pwm_l, duty_l)
+        return
+        
+    def execute(self):
+        # 処理時間計測
+        start_time = datetime.now()
+        self.prev_encoder_values['r'] = self.encoder_values['r']
+        self.prev_encoder_values['l'] = self.encoder_values['l']
+        try:
+            while (self.encoder_values['l'] <= Control.encoder_1rotation_l*Control.rotation_num):
+                # 時間更新
+                current_time = time.time()
+                dt = current_time - self.prev_time
+                self.prev_time = time.time()
+                # ログ
+                elapsed_time = datetime.now() - start_time
+                elapsed_seconds = round(float(elapsed_time.total_seconds()), 4)
+                Fig.time_data.append(elapsed_seconds)
+                # モータの目標角速度と電流値を計算
+                w_r, w_l, i_r, i_l= self.calc_target_w_i(Control.v_target, Control.w_target, Control.a_target, Control.alpha_target, dt)
+                # 0.01秒周期でモータに指令を送る
+                if(current_time - self.prev_time_for_pwm > 0.01):
+                    self.motor_control(w_r, w_l, i_r, i_l)
+                    print(f"self.encoder_values['l']: {self.encoder_values['l']}")
+                time.sleep(PID.dt)
+        finally:
+            self.pwm_control(Pin.pwm_r, 0)
+            self.pwm_control(Pin.pwm_l, 0)
+            self.ax1.plot(Fig.time_data, Fig.vel_data, color="blue", label="vel_data")
+            self.ax1.plot(Fig.time_data, Fig.target_vel_data, color="green", label="target_vel")
+            self.ax2.plot(Fig.time_data, Fig.w_data, color="red", label="w_data")
+            self.ax2.plot(Fig.time_data, Fig.target_w_data, color="green", label="target_w")
+            self.ax1.legend(loc = 'upper right')
+            self.ax2.legend(loc = 'upper right')
+            self.ax1.grid(True)
+            self.ax2.grid(True)
+            self.fig.tight_layout()
+            now = datetime.now().replace(microsecond=0)
+            plt.savefig(f"/home/ubuntu/ros1_ws/src/tang/test/graph/{now}_v={Control.v_target}_a={Control.a_target}_w={Control.w_target}_α={Control.alpha_target}.png")
+            self.pi.stop()
+
+def main():
+    motor = Motor()
+    motor.execute()
+    
+if __name__ == "__main__":
+    main()
